04888fc335198814bc63c7eb6f484a3c96921809
Mark Fejes <fejes.mark92@gmail.com>
2017-07-27 23:00:08 +0200

    refactor runner.js async await

--- api/runner/runner.js
+++ api/runner/runner.js
@@ -2,36 +2,31 @@ const newman = require('newman');
 const testsFolder = '../tests/';
 const fs = require('fs');
 const testFiles = [];
-const async = require('async');
+const { promisify } = require('util');
 
 fs.readdirSync(testsFolder).forEach(file => {
     if(file.includes('api.postman-collection')){
         testFiles.push(file);
     }
-})
- 
-async.mapSeries(testFiles,
-  // item iterator function
-  (testFile, callback) => {
-    console.log('Testing now: ' + testFile);
-    newman.run({
-      collection:   testsFolder+testFile,
-      environment:  testsFolder+'environment.json',
-      reporters:        ['cli','html','junit'],
-      reporter: { html: { export : '../output/'+testFile+'.html'}, junit: { export : '../junit/'+testFile+'.xml'}}
-    }, (err, result) => {
-      if (err) {
-        return callback(err);
-      }
-      callback(null, result);
-    });
-  },
-  // callback function, when all iterate function finished (or error happened)
-  (err, results) => {
-    if (err) {
-      console.log('Something went wrong while running tests', err);
-      return;
+});
+
+async function main () {
+  try {
+    const results = [];
+    for (const testFile of testFiles) {
+      const result = await promisify(newman.run)({
+        collection:   testsFolder+testFile,
+        environment:  testsFolder+'environment.json',
+        reporters:        ['cli','html','junit'],
+        reporter: { html: { export : '../output/'+testFile+'.html'}, junit: { export : '../junit/'+testFile+'.xml'}}
+      });
+      results.push(result);
     }
-    console.log('reports');
     console.log(results);
-  });
+  } catch (err) {
+    console.log('Something went wrong while running tests', err);
+  }
+}
+main();
+
+
